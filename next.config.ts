a07719e6be82b439df9c5676755bfd5e8d1de6f5
kaneabdel-droid <kaneabdel@gmail.com>
2026-09-04 13:07:34 +0000

    Fix next.config.ts build error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    // Avertissement : cela permet aux builds de production de réussir avec succès
-    // même s'il y a des erreurs ESLint.
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
